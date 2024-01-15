75439012d207f2f2a2416e82511e87cb7465655d
sachin27verma <97831442+sachin27verma@users.noreply.github.com>
2024-01-15 17:28:51 +0530

    change according

--- client/src/app/personalisedhealthcare/page.jsx
+++ client/src/app/personalisedhealthcare/page.jsx
@@ -150,19 +150,19 @@ const MentalHealth = () => {
           <Markdown remarkPlugins={[remarkGfm]} 
           components={{
             code(props) {
-              const {children, className, node, ...rest} = props
+              const {betakyahua, className, node, ...rest} = props
               const match = /language-(\w+)/.exec(className || '')
               return match ? (
                 <SyntaxHighlighter
                  {...rest}
                   PreTag="p"
-                  children={String(children).replace(/\n$/, '')}
+                  children={String(betakyahua).replace(/\n$/, '')}
                   language={match[1]}
                   style={dark}
                 />
               ) : (
                 <code {...rest} className={className}>
-                  <children/>
+                  {betakyahua}
                 </code>
               )
             }

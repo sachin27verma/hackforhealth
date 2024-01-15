c8aa939cc06f44c49bd3f73855492b0e03efa41c
sachin27verma <97831442+sachin27verma@users.noreply.github.com>
2024-01-15 09:44:43 +0530

    cheaking git init

--- server/server.py
+++ server/server.py
@@ -17,23 +17,23 @@ def return_home():
 
 @app.route('/personal', methods=['POST'])
 def predict_health():
-    print("server ke pass hu")
+    # print("server ke pass hu")
     data = request.get_json()  # Get input data from the frontend'
     # formdata=request.form.values()
     # print("formdata",formdata)
     # int_value=[int(x) for x in request.form.values() ]
     # print("array",data.data)
-    print("Received input_data:", data)
+    # print("Received input_data:", data)
     input_data = np.array(data).reshape(1, 102)
-    print("data",input_data)
+    # print("data",input_data)
     # print("intergr data",int_value)
     # features=[np.array(int_value)]
     
     prediction = model.predict(input_data)  # Make prediction using your model
     # Accuracy=accuracy_score(prediction,y_pred)
     # print("Accuracy",Accuracy)
-    print("predicted",prediction[0])
-    print("predicted value",prediction[0])
+    # print("predicted",prediction[0])
+    # print("predicted value",prediction[0])
     return jsonify({'result': prediction[0]})  # Send prediction back to frontend
 
 if __name__=='__main__':
